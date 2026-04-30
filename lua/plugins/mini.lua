218bf2c7e0faf3a842d9a5cbe04d7947295ffb6e
Yohannes <y.berhane56@gmail.com>
2026-04-30 16:42:04 -0400

    Changes to Mini for now

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -7,24 +7,22 @@ return {
       --  - va)  - [V]isually select [A]round [)]parenthenses
       --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup {
-        mappings = {
-          around_next = 'aa',
-          inside_next = 'ii',
-        },
-        n_lines = 500,
-      }
+      -- require('mini.ai').setup {
+      --   mappings = {
+      --     around_next = 'aa',
+      --     inside_next = 'ii',
+      --   },
+      --   n_lines = 500,
+      -- }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]parentheses
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
