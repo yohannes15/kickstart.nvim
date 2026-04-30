a61bd16eaed58b13389145a3b6912429eda2c5bf
Yohannes <y.berhane56@gmail.com>
2026-04-30 02:48:33 -0400

    Readd Mini

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,7 @@ return {
   unpack(require 'plugins.colorscheme'),
   unpack(require 'plugins.treesitter'),
   unpack(require 'plugins.render_markdown'),
+  unpack(require 'plugins.mini'),
   unpack(require 'plugins.snacks'),
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
